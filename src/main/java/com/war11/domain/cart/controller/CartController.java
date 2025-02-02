27ce8a05a2f0dee97e7741052f4d637597bc20b9
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-03 01:41:46 +0900

    ✨ add create cart logic

--- src/main/java/com/war11/domain/cart/controller/CartController.java
+++ src/main/java/com/war11/domain/cart/controller/CartController.java
@@ -1,4 +1,27 @@
 package com.war11.domain.cart.controller;
 
+import com.war11.domain.cart.dto.response.CartResponse;
+import com.war11.domain.cart.service.CartService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/carts")
 public class CartController {
+
+  private final CartService cartService;
+
+  @PostMapping
+  public ResponseEntity<CartResponse> createCartApi(){
+    CartResponse response = cartService.createCart();
+
+    return new ResponseEntity<>(response, HttpStatus.OK);
+  }
 }
--- src/main/java/com/war11/domain/cart/service/CartService.java
+++ src/main/java/com/war11/domain/cart/service/CartService.java
@@ -1,4 +1,25 @@
 package com.war11.domain.cart.service;
 
+import com.war11.domain.cart.dto.request.CartRequest;
+import com.war11.domain.cart.dto.response.CartResponse;
+import com.war11.domain.cart.entity.Cart;
+import com.war11.domain.cart.repository.CartRepository;
+import com.war11.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class CartService {
+
+  private final CartRepository cartRepository;
+
+  // Todo: User new로 만들어서 넣지 않고 토큰에서 뽑아 넣도록 수정하기
+  public CartResponse createCart() {
+    Cart cart = new Cart(new User());
+    cartRepository.save(cart);
+    return new CartResponse("카트가 생성되었습니다.");
+  }
 }
