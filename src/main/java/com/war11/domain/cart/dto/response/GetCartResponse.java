89cb2ec5b7385d8cd62ca61948f879c534ba4839
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-03 14:53:42 +0900

    ✨ modify field or remove class to add GetCart method

--- src/main/java/com/war11/domain/cart/dto/response/GetCartResponse.java
+++ src/main/java/com/war11/domain/cart/dto/response/GetCartResponse.java
@@ -3,9 +3,7 @@ package com.war11.domain.cart.dto.response;
 import java.util.List;
 
 public record GetCartResponse(
-    Long userId,
-    Long cartId,
-    List<GetCartProductResponse> cartProducts
+    List<CartProductResponse> cartProducts
 ) {
 
 }
