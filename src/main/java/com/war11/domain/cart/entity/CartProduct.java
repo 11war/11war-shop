d9166508918ce68bc1be65519979ddcd040a8ffe
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-03 14:54:30 +0900

    ✨ add Valid, Constructor, update and toggleCheck logic

--- src/main/java/com/war11/domain/cart/entity/CartProduct.java
+++ src/main/java/com/war11/domain/cart/entity/CartProduct.java
@@ -2,12 +2,14 @@ package com.war11.domain.cart.entity;
 
 import com.war11.domain.product.entity.Product;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CartProduct {
 
   @Id
@@ -22,7 +24,24 @@ public class CartProduct {
   @JoinColumn(name = "product_id")
   private Product product;
 
+  @Column(nullable = false)
   private Integer quantity;
 
+  @Column(nullable = false)
   private boolean isChecked;
+
+  public CartProduct(Cart cart, Product product, Integer quantity, boolean isChecked) {
+    this.cart = cart;
+    this.product = product;
+    this.quantity = quantity;
+    this.isChecked = isChecked;
+  }
+
+  public void updateQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  public void toggleCheck() {
+    isChecked = !isChecked;
+  }
 }
