c4d34627e961752a8199533135a2cfb89e7b55f0
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-04 10:33:20 +0900

    ✨ modify response Entity

--- src/main/java/com/war11/domain/cart/controller/CartController.java
+++ src/main/java/com/war11/domain/cart/controller/CartController.java
@@ -6,6 +6,7 @@ import com.war11.domain.cart.dto.response.CartProductResponse;
 import com.war11.domain.cart.dto.response.CartResponse;
 import com.war11.domain.cart.dto.response.GetCartResponse;
 import com.war11.domain.cart.service.CartService;
+import com.war11.global.common.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,47 +31,47 @@ public class CartController {
 
   // Todo: userId 패스배리어블 말고 토큰에서 빼도록 수정
   @PostMapping("/users/{userId}/products/{productId}")
-  public ResponseEntity<Void> addToCartApi(@RequestBody AddCartProductRequest request,
+  public ResponseEntity<ApiResponse<Void>> addToCartApi(@RequestBody AddCartProductRequest request,
       @PathVariable Long userId, @PathVariable Long productId) {
     cartService.addToCart(request, userId, productId);
 
-    return new ResponseEntity<>(HttpStatus.OK);
+    return ApiResponse.noContentAndSendMessage("장바구니에 상품이 추가되었습니다.");
   }
 
   // Todo: userId 패스배리어블 말고 토큰에서 빼도록 수정
   @GetMapping("/users/{id}")
-  public ResponseEntity<GetCartResponse> getCartApi(@PathVariable(name = "id") Long userId) {
+  public ResponseEntity<ApiResponse<GetCartResponse>> getCartApi(@PathVariable(name = "id") Long userId) {
     GetCartResponse response = cartService.getCart(userId);
 
-    return new ResponseEntity<>(response, HttpStatus.OK);
+    return ApiResponse.success(response);
   }
 
   @PatchMapping("/cart-products/{id}/quantity")
-  public ResponseEntity<CartProductResponse> updateQuantityApi(
+  public ResponseEntity<ApiResponse<CartProductResponse>> updateQuantityApi(
       @PathVariable(name = "id") Long cartProductId,
       @Valid @RequestBody UpdateCartProductRequest request) {
     CartProductResponse response = cartService.updateQuantity(cartProductId, request);
-    return new ResponseEntity<>(response, HttpStatus.OK);
+    return ApiResponse.success(response);
   }
 
   @PatchMapping("/cart-products/{id}/checked")
-  public ResponseEntity<CartProductResponse> toggleCheckedApi(
+  public ResponseEntity<ApiResponse<CartProductResponse>> toggleCheckedApi(
       @PathVariable(name = "id") Long cartProductId) {
     CartProductResponse response = cartService.toggleChecked(cartProductId);
-    return new ResponseEntity<>(response, HttpStatus.OK);
+    return ApiResponse.success(response);
   }
 
   @DeleteMapping("/cart-product/{id}")
-  public ResponseEntity<Void> deleteCartProductApi(@PathVariable(name = "id") Long cartProductId) {
+  public ResponseEntity<ApiResponse<Void>> deleteCartProductApi(@PathVariable(name = "id") Long cartProductId) {
     cartService.deleteCartProduct(cartProductId);
 
-    return new ResponseEntity<>(HttpStatus.OK);
+    return ApiResponse.noContentAndSendMessage("장바구니에서 상품이 삭제되었습니다.");
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> deleteCartApi(@PathVariable(name = "id") Long id) {
+  public ResponseEntity<ApiResponse<Void>> deleteCartApi(@PathVariable(name = "id") Long id) {
     cartService.deleteCart(id);
 
-    return new ResponseEntity<>(HttpStatus.OK);
+    return ApiResponse.noContentAndSendMessage("장바구니가 비어있습니다.");
   }
 }
