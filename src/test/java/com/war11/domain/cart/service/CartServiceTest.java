26e4285b3233fc8e547c052ad343d31cc7f2dec5
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-05 05:39:39 +0900

    ✨ add mock instances and adding product logic when the cart is already exist

--- src/test/java/com/war11/domain/cart/service/CartServiceTest.java
+++ src/test/java/com/war11/domain/cart/service/CartServiceTest.java
@@ -40,14 +40,20 @@ class CartServiceTest {
 
   private User user;
   private Product product1;
-//  private Product product2;
+  private Product product2;
+  private Cart cart;
+  private CartProduct cartProduct1;
+  private CartProduct cartProduct2;
 
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
     user = mock(User.class);
     product1 = mock(Product.class);
-//    product2 = mock(Product.class);
+    product2 = mock(Product.class);
+    cart = mock(Cart.class);
+    cartProduct1 = mock(CartProduct.class);
+    cartProduct2 = mock(CartProduct.class);
   }
 
 
@@ -67,7 +73,6 @@ class CartServiceTest {
     cartService.addToCart(request, 1L, 1L);
 
     // then
-    // 저장된 카트의 유저가 1L 유저와 같은지 확인
     ArgumentCaptor<Cart> cartCaptor = ArgumentCaptor.forClass(Cart.class);
     verify(cartRepository).save(cartCaptor.capture());
     Cart savedCart = cartCaptor.getValue();
@@ -77,5 +82,35 @@ class CartServiceTest {
     verify(cartProductRepository).save(cartProductCaptor.capture());
     CartProduct savedCartProduct = cartProductCaptor.getValue();
     assertThat(savedCartProduct.getProduct().getId()).isEqualTo(1L);
+  }
+
+  @Test
+  void 장바구니_존재할때_아이템추가() {
+    // given: dto 생성, user/product/cart id 부여, 카트/프로덕트 검색결과 부여
+    AddCartProductRequest request = new AddCartProductRequest(2, true);
+
+    when(user.getId()).thenReturn(1L);
+    when(product1.getId()).thenReturn(1L);
+    when(cart.getId()).thenReturn(1L);
+
+    when(cartRepository.findCartByUserId(user.getId())).thenReturn(Optional.of(cart));
+    when(productRepository.findById(product1.getId())).thenReturn(Optional.of(product1));
+
+    // when: 메서드 실행
+    cartService.addToCart(request, 1L, 1L);
+
+    // then
+    // 장바구니 저장 메서드가 실행된적이 없는지(never) 확인
+    verify(cartRepository, never()).save(any());
+
+    // 새로운 상품이 장바구니에 정상적으로 추가되었는지 확인
+    ArgumentCaptor<CartProduct> cartProductCaptor = ArgumentCaptor.forClass(CartProduct.class);
+    verify(cartProductRepository).save(cartProductCaptor.capture());
+    CartProduct savedCartProduct = cartProductCaptor.getValue();
+
+    assertThat(savedCartProduct.getCart()).isEqualTo(cart);
+    assertThat(savedCartProduct.getProduct()).isEqualTo(product1);
+    assertThat(savedCartProduct.getQuantity()).isEqualTo(2);
+    assertThat(savedCartProduct.isChecked()).isTrue();
   }
 }
