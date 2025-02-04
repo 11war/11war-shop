f8efc61109b09e1a3bf39c2ce135265eabf4a4e2
lq0920084 <lq0920084@gmail.com>
2025-02-04 10:58:57 +0900

    🐛: add Product ErrorCode

--- src/main/java/com/war11/global/exception/enums/ErrorCode.java
+++ src/main/java/com/war11/global/exception/enums/ErrorCode.java
@@ -21,9 +21,12 @@ public enum ErrorCode {
 
     //User
     USER_ID_ERROR(HttpStatus.UNAUTHORIZED,"1","아이디나 비밀번호가 올바르지 않습니다."),
-    USER_PW_ERROR(HttpStatus.UNAUTHORIZED,"2","아이디나 비밀번호가 올바르지 않습니다.");
+    USER_PW_ERROR(HttpStatus.UNAUTHORIZED,"2","아이디나 비밀번호가 올바르지 않습니다."),
+
 
     //Product
+    NOT_FOUND_PRODUCT_ID(HttpStatus.NOT_FOUND,"","찾는 상품이 없습니다."),
+    INVALID_PRODUCT_STATUS_VALUE(HttpStatus.BAD_REQUEST,"" ,"해당하는 상태코드가 없습니다." );
 
     //Cart
 
