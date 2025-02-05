2e3732ef03558a542ee6e8c14c1423c3b4bcf9b1
lq0920084 <lq0920084@gmail.com>
2025-02-05 16:09:20 +0900

    🐛: valid 오류 수정

--- src/main/java/com/war11/domain/product/dto/request/ProductUpdateRequest.java
+++ src/main/java/com/war11/domain/product/dto/request/ProductUpdateRequest.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.war11.domain.product.entity.Product;
 import com.war11.domain.product.entity.enums.ProductStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 
 @Builder
 public record ProductUpdateRequest(
-    @NotBlank(message = "상품고유번호는 빈값일 수 없습니다.")
+    @Min(value = 1, message = "상품고유번호는 빈값일 수 없습니다.")
     @JsonProperty("id")
     Long id,
     @NotBlank(message = "상품명은 빈값일 수 없습니다.")
@@ -17,13 +19,13 @@ public record ProductUpdateRequest(
     @NotBlank(message = "카테고리명은 빈값일 수 없습니다.")
     @JsonProperty("category")
     String category,
-    @NotBlank(message = "가격은 빈값일 수 없습니다.")
+    @Min(value = 0, message = "가격은 비어있거나 0보다 작을 수 없습니다.")
     @JsonProperty("price")
     Long price,
-    @NotBlank(message = "수량은 빈값일 수 없습니다.")
+    @Min(value = 0, message = "수량은 비어있거나 0보다 작을 수 없습니다.")
     @JsonProperty("quantity")
     int quantity,
-    @NotBlank(message = "상품상태는 빈값일 수 없습니다.")
+    @NotNull(message = "상품상태는 빈값일 수 없습니다.")
     @JsonProperty("status")
     ProductStatus status
 ) {
