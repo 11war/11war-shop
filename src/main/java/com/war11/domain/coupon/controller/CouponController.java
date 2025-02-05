caf150b008bb4e9e7fce59b522bed2d0f0f7e664
chk223 <kch23_71@naver.com>
2025-02-05 11:32:40 +0900

    🩹 유저 정보를 security에서 가져올 수 있도록 변경

--- src/main/java/com/war11/domain/coupon/controller/CouponController.java
+++ src/main/java/com/war11/domain/coupon/controller/CouponController.java
@@ -3,9 +3,11 @@ package com.war11.domain.coupon.controller;
 import com.war11.domain.coupon.dto.response.CouponResponse;
 import com.war11.domain.coupon.service.CouponService;
 import com.war11.global.common.ApiResponse;
+import com.war11.global.config.CustomUserDetails;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,14 +21,14 @@ public class CouponController {
     private final CouponService couponService;
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<CouponResponse>>> getMyCoupons(Long userId) {
-        List<CouponResponse> userCoupons = couponService.findUserCoupons(userId);
+    public ResponseEntity<ApiResponse<List<CouponResponse>>> getMyCoupons(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        List<CouponResponse> userCoupons = couponService.findUserCoupons(userDetails.getId());
         return ApiResponse.success(userCoupons);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<CouponResponse>> getMyCouponById(@PathVariable Long id, Long userId) {
-        CouponResponse userCoupon = couponService.findUserCoupon(id, userId);
+    public ResponseEntity<ApiResponse<CouponResponse>> getMyCouponById(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        CouponResponse userCoupon = couponService.findUserCoupon(id, userDetails.getId());
         return ApiResponse.success(userCoupon);
     }
 
