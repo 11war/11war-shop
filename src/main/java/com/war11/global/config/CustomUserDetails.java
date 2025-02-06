ff3519517337e449d70cbc3b704e9e929cfb7712
juyangjin <wndid2008@gmail.com>
2025-02-07 01:39:08 +0900

    :zap: 유저 역할 추가 (기본적으로 권한을 유저로 설정)

--- src/main/java/com/war11/global/config/CustomUserDetails.java
+++ src/main/java/com/war11/global/config/CustomUserDetails.java
@@ -2,8 +2,10 @@ package com.war11.global.config;
 
 import com.war11.domain.user.entity.User;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class CustomUserDetails implements UserDetails {
@@ -14,9 +16,15 @@ public class CustomUserDetails implements UserDetails {
         this.user = user;
     }
 
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        // user.getRole()이 null이 아니면 "ROLE_"를 붙여서 반환
+        String role = user.getRole();
+        if (role == null) {
+            return List.of();
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
     }
 
     @Override
