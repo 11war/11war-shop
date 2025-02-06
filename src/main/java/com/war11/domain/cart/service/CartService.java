056cba17f4731300b2ec9f14bc9a9b3c36f8a639
DESKTOP-5S4I34C\PC <qkdehdgur3@gmail.com>
2025-02-06 12:56:33 +0900

    ✨ add validate method and findEntity method

--- src/main/java/com/war11/domain/cart/service/CartService.java
+++ src/main/java/com/war11/domain/cart/service/CartService.java
@@ -11,10 +11,14 @@ import com.war11.domain.cart.repository.CartRepository;
 import com.war11.domain.product.entity.Product;
 import com.war11.domain.product.repository.ProductRepository;
 import com.war11.domain.user.repository.UserRepository;
+import com.war11.global.exception.BusinessException;
+import com.war11.global.exception.base.NotFoundException;
+import com.war11.global.exception.enums.ErrorCode;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -36,13 +40,10 @@ public class CartService {
    * ref. 반환값이 없어서 request에서 toDto메서드 사용.
    */
   public void addToCart(AddCartProductRequest request, Long userId, Long productId) {
-    Cart foundCart = cartRepository.findCartByUserId(userId)
-        .orElseGet(() -> {
-          Cart newCart = new Cart(userRepository.findById(userId).orElseThrow());
-          return cartRepository.save(newCart);
-        });
-
-    Product foundProduct = productRepository.findById(productId).orElseThrow();
+    Product foundProduct = productRepository.findById(productId).orElseThrow(
+        () -> new NotFoundException(ErrorCode.PRODUCT_NOT_FOUND)
+    );
+    Cart foundCart = validateCartAndCheckStock(userId, request, foundProduct);
 
     CartProduct cartProduct = request.toEntity(foundCart, foundProduct);
     cartProductRepository.save(cartProduct);
@@ -56,7 +57,9 @@ public class CartService {
    * 4. {@code cartId}, {@code foundCartProducts}리스트로 {@code GetCarResponse}생성 후 반환
    */
   public GetCartResponse getCart(Long userId) {
-    Cart foundCart = cartRepository.findCartByUserId(userId).orElseThrow();
+    Cart foundCart = cartRepository.findCartByUserId(userId).orElseThrow(
+        () -> new NotFoundException(ErrorCode.CART_NOT_FOUND)
+    );
 
     List<CartProductResponse> foundCartProducts = cartProductRepository.findCartProductByCartId(
         foundCart.getId()).stream().map(CartProduct::toDto).toList();
@@ -66,7 +69,8 @@ public class CartService {
 
   @Transactional
   public CartProductResponse updateQuantity(Long cartProductId, UpdateCartProductRequest request) {
-    CartProduct foundCartProduct = cartProductRepository.findById(cartProductId).orElseThrow();
+    CartProduct foundCartProduct = findEntity(cartProductRepository, cartProductId,
+        ErrorCode.PRODUCT_NOT_FOUND);
 
     foundCartProduct.updateQuantity(request.getQuantity());
 
@@ -75,7 +79,8 @@ public class CartService {
 
   @Transactional
   public CartProductResponse toggleChecked(Long cartProductId) {
-    CartProduct foundCartProduct = cartProductRepository.findById(cartProductId).orElseThrow();
+    CartProduct foundCartProduct = findEntity(cartProductRepository, cartProductId,
+        ErrorCode.PRODUCT_NOT_FOUND);
 
     foundCartProduct.toggleCheck();
 
@@ -84,18 +89,54 @@ public class CartService {
 
   @Transactional
   public void deleteCartProduct(Long cartProductId) {
-    CartProduct foundCartProduct = cartProductRepository.findById(cartProductId).orElseThrow();
+    CartProduct foundCartProduct = findEntity(cartProductRepository, cartProductId,
+        ErrorCode.PRODUCT_NOT_FOUND);
     Long cartId = foundCartProduct.getCart().getId();
+
     cartProductRepository.delete(foundCartProduct);
 
-    if (cartProductRepository.findCartProductByCartId(cartId).isEmpty()) {
+    if (!cartProductRepository.existsByCartId(cartId)) {
       deleteCart(cartId);
     }
   }
 
   @Transactional
   public void deleteCart(Long cartId) {
-    Cart foundCart = cartRepository.findById(cartId).orElseThrow();
+    Cart foundCart = findEntity(cartRepository, cartId, ErrorCode.CART_NOT_FOUND);
     cartRepository.delete(foundCart);
+  }
+
+  /**
+   * 카트 존재유무, 상품 재고 검증 메서드 <br>
+   * 1. {@code userId}, {@code request}, {@code product} 받음. <br>
+   * 2. {@code request}와 {@code product}에서 재고 비교. 현재 재고보다 많은 수량 요청 시 예외 발생 <br>
+   * 3. {@code userId}로 카트 찾고 반환. 없으면 생성 후 저장해서 반환. <br>
+   */
+  public Cart validateCartAndCheckStock(
+      Long userId, AddCartProductRequest request, Product product) {
+
+    if (request.getQuantity() > product.getQuantity()) {
+      throw new BusinessException(ErrorCode.OUT_OF_STOCK);
+    }
+
+    Cart foundCart = cartRepository.findCartByUserId(userId)
+        .orElseGet(() -> {
+          Cart newCart = new Cart(userRepository.findById(userId).orElseThrow(
+              () -> new NotFoundException(ErrorCode.CART_NOT_FOUND)
+          ));
+          return cartRepository.save(newCart);
+        });
+    return foundCart;
+  }
+
+  /**
+   * 제네릭타입 find 메서드 <br>
+   * 타입 별로 {@code repository}, {@code id}, {@code errorCode}대입받음. <br>
+   * 각 레포지토리에서 {@code id}기준으로 탐색, 없을 시 예외 발생. <br>
+   */
+  private <T> T findEntity(JpaRepository<T, Long> repository, Long id, ErrorCode errorCode) {
+    return repository.findById(id).orElseThrow(
+        () -> new NotFoundException(errorCode)
+    );
   }
 }
