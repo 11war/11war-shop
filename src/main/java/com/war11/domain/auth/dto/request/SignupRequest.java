1b05cd51bcfa2cd09d7449a8ef2b426a7813f632
juyangjin <wndid2008@gmail.com>
2025-02-07 11:28:05 +0900

    :zap: 비밀번호 패턴 추가

--- src/main/java/com/war11/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/com/war11/domain/auth/dto/request/SignupRequest.java
@@ -2,6 +2,7 @@ package com.war11.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,5 +17,6 @@ public class SignupRequest {
     @NotBlank
     private String name;
     @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,}", message = "비밀번호는 최소 8자 이상이며, 숫자, 영문자, 특수문자를 모두 포함해야 합니다.")
     private String password;
 }
